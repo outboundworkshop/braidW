dca1217d2c8e81c3ae76a5e86483d3b486572fa5
kimsee <ttungseui11@gmail.com>
2025-05-31 12:25:37 +0900

    import update

--- src/main/java/com/example/braidw/config/OpenApiConfig.java
+++ src/main/java/com/example/braidw/config/OpenApiConfig.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+// 스웨거 설정
 @Configuration
 public class OpenApiConfig {
     
